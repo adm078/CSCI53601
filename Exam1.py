5c61388b40aed5470424cc4f1e4351fb93e85fd4
ayowb <ayowb12@gmail.com>
2024-09-23 00:30:38 -0400

    exam file added

---
+++ Exam1.py
@@ -0,0 +1,68 @@
+#Exam tmw
+# print("ayoub")
+
+
+# name = str(input("enter your name: "))
+# con = bool(True)
+# phone = int(input("Enter your number: "))
+
+
+# con = bool(True)
+# print(con)
+
+    # ****************** Part 1 ****************
+# while (con):
+#      if name == 'yaman':
+#          print("Hello Ayoub ! Weclome Home")
+#          con = False
+       
+#      else:
+#          con = True
+#          print("Who are you !")
+#          name = input("Enter your name again: ")
+
+# part 2
+# while (con):
+    
+#    if name.isdigit():
+#      print("your name cant be a number")
+#      name = input("enter your name again")
+#      continue
+
+#    else:
+#       name = str(input("thank you"))
+#       con= False
+
+# nam1 = "ayoub"
+# pho1 = 843739618
+# def showmeresult () :
+#    print("im inside the fuction")
+#    print("Hello my name is"+ nam1 + "and my phone number is"+ str(pho1))
+#    print("****************")
+#    print("Hello my name is", nam1 , "and my phone number is")
+#    print("im about to quit th function")
+   
+
+
+# if name == nam1:
+#     showmeresult()
+# else:
+#     print("values dosne't match")
+    
+
+# def calculations (N1 , N2 , N3):
+#     if N1 > N2 and N1 > N3:
+#         print(N1, "is the largest number")
+#     elif N2> N1 and N2 > N3:
+#         print(N2, "is the largest number")
+        
+# return N3
+   
+        
+# calculations(98, 30, 24)
+
+# for i in 'Findlay':
+#     print(i)
+x=int(100)
+for i in enumerate(2,2):
+    print(i)
